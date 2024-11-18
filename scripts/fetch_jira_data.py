1fc7d45b6cc2125779a6c8530f07d1de57443ac3
Arnold C Gomez <86652814+arnoldcgomez@users.noreply.github.com>
2024-11-18 15:52:50 +0530

    Update fetch_jira_data.py

--- scripts/fetch_jira_data.py
+++ scripts/fetch_jira_data.py
@@ -42,7 +42,7 @@ def main():
                     "issuetype": issue["fields"].get("issuetype"),
                     "project": issue["fields"].get("project"),
                     "assignee": issue["fields"].get("assignee", {}).get("displayName"),
-                    "description": issue["description"].get("description")
+                    "description": issue["fields"].get("description")
                 }
                 filtered_issues.append(filtered_data)
 
