d56758035ccec5552f336681647556325eb67fb6
Arnold C Gomez <86652814+arnoldcgomez@users.noreply.github.com>
2024-11-26 11:46:30 +0530

    Update create_jira_tasks.py

--- scripts/create_jira_tasks.py
+++ scripts/create_jira_tasks.py
@@ -1,1 +1,22 @@
+import sys
+import pandas as pd
+from jira import JIRA
+import os
 
+def main(file_path)
+    jira_server = os.getenv("JIRA_DOMAIN")
+    jira_user = os.getenv("JIRA_USERNAME")
+    jira_api_token = os.getenv("JIRA_API_TOKEN")
+    jira_project_key = os.getenv("JIRA_PROJECT_KEY")
+
+    jira = JIRA(server=jira_server, basic_auth=(jira_user,jira_api_token))
+
+    try:
+      tasks = pd.read_excel(file_path)
+    except Exception as e:
+      print(f"Error reading Excel file: {e}")
+      sys.exit(1)
+
+    required_columns = ["Summary", "Description", "issue Type"]
+    if not all(column in tasks.columns for column in required_columns):
+      print(f"Excel must contain the following columns: {}")
